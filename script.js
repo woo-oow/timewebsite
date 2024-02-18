9f5d78fa4c818e4ee55a58a206d59bb1d8cb0629
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-18 14:41:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ function updateDateTime() {
     const now = new Date();
 }
 
-function changeFormat() {
-    const datetime = document.getElementById('datetime');
-    analog = !analog
+
+function changeFormat(format) {
+    // You can use the `format` argument here, for example:
+    if (format === 'analog') {
+        // Do something for analog format
+        alert('Switching to analog format!');
+    } else {
+        // Do something for other formats
+        alert('Switching to another format!');
+    }
 }
 
 
