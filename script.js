b46a67d433af640e6263231557a5353824e96699
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-19 11:17:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 let sys1 = false;
 
 function updateClock() {
-    if (sys1) {
-        const now = new Date();
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
-        const timeString = `${hours}: ${minutes}: ${seconds}`;
+
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const currtime = `${hours}: ${minutes}: ${seconds}`;
     
+    if (sys1) {
 
-        document.getElementById('clock-time').textContent = timeString;
-    }    
-    else {
-        const now = new Date();
-        const hours = String(now.getHours()).padStart(2, '0') % 12;
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
-        const timeString = `${hours}:${minutes}:${seconds}`;
-
-
+        hours %= 12
+    
         if (hours <= 12) {
-            const timeString = `${hours}: ${minutes}: ${seconds} am`;
+            const currtime = `${hours}: ${minutes}: ${seconds} am`;
         else {
-             const timeString = `${hours}: ${minutes}: ${seconds} pm`;
+             const currtime = `${hours}: ${minutes}: ${seconds} pm`;
         }
-        
-        document.getElementById('clock-time').textContent = timeString;
+
     }
+    document.getElementById('clock-time').textContent = timeString;
 }
 
 function swap() {
     sys1 = !sys1;
+    updateClock();
 }
 
 updateClock(); 
