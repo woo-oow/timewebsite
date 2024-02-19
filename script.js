e8c852336c1b4f58a269d5c49f9b9b8056583370
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-19 11:55:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function update() {
 
     const now = new Date();
     let hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');a
+    const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
     let currtime = `${hours}: ${minutes}: ${seconds}`;
 
