7c71337e8803c62685c4d091bb36b7d8e18efe2e
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-19 15:04:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 let sys1 = false;
 
 function update() {
-
+    
     const now = new Date();
-    let hours = String(now.getHours());
-    const minutes = String(now.getMinutes());
-    const seconds = String(now.getSeconds());
-    let currtime = `${hours}: ${minutes}: ${seconds}`;
+    let hours = now.getHours();
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    let currtime = `${hours}:${minutes}:${seconds}`;
     document.getElementById('sys').textContent = '24hr';
 
     if (sys1) {
-
         
         hours %= 12;
         currtime += hours < 12 ? ' am' : ' pm';
         document.getElementById('sys').textContent = '12hr';
-        
     }
-    
+
     document.getElementById('clock-time').textContent = currtime;
-    
 }
 
 function swap() {
@@ -27,5 +24,5 @@ function swap() {
     update();
 }
 
-update(); 
-setInterval(update, 1000); 
+update();
+setInterval(update, 1000);
