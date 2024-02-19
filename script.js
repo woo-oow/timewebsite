5ba7f7ac18d36b9ff94e52af89d2338a751ca079
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-18 21:58:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+let sys1 = false
+
 function updateClock() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('clock-time').textContent = timeString;
+    if (sys1) {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+        document.getElementById('clock-time').textContent = timeString;
+    }
+    else {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0') % 12;
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+        document.getElementById('clock-time').textContent = timeString;
+    }
+
+}
+
+function swap() {
+    sys1 = !sys1
 }
 
 updateClock(); // Update the clock immediately
