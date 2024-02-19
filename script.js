6e065c86c046424bcb33f97c13d40c3a9cdf3dcb
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-19 11:11:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ function updateClock() {
         const minutes = String(now.getMinutes()).padStart(2, '0');
         const seconds = String(now.getSeconds()).padStart(2, '0');
         const timeString = `${hours}: ${minutes}: ${seconds}`;
-        
-        
-        if (hours <= 12) {
-            const timeString = `${hours}: ${minutes}: ${seconds} am`;
-        else {
-             const timeString = `${hours}: ${minutes}: ${seconds} pm`;
-        }
+    
 
         document.getElementById('clock-time').textContent = timeString;
     }    
@@ -23,6 +17,13 @@ function updateClock() {
         const minutes = String(now.getMinutes()).padStart(2, '0');
         const seconds = String(now.getSeconds()).padStart(2, '0');
         const timeString = `${hours}:${minutes}:${seconds}`;
+
+
+        if (hours <= 12) {
+            const timeString = `${hours}: ${minutes}: ${seconds} am`;
+        else {
+             const timeString = `${hours}: ${minutes}: ${seconds} pm`;
+        }
         
         document.getElementById('clock-time').textContent = timeString;
     }
