811be492c26cadd600a42235cd9541522fc90150
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-18 20:39:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-var analog = false;
+let timerInterval;
+let stopwatchInterval;
+let stopwatchTime = 0;
 
-function updateDateTime() {
-    const now = new Date();
+function startTimer() {
+  const timerInput = document.getElementById("timerInput");
+  const timeInSeconds = parseInt(timerInput.value);
+  if (!isNaN(timeInSeconds) && timeInSeconds > 0) {
+    timerInterval = setInterval(() => {
+      timeInSeconds--;
+      if (timeInSeconds < 0) {
+        clearInterval(timerInterval);
+        alert("Timer finished!");
+      }
+    }, 1000);
+  }
 }
 
+function stopTimer() {
+  clearInterval(timerInterval);
+}
 
-updateDateTime();
+function startStopwatch() {
+  stopwatchInterval = setInterval(() => {
+    stopwatchTime++;
+    updateStopwatchDisplay();
+  }, 1000);
+}
 
+function stopStopwatch() {
+  clearInterval(stopwatchInterval);
+}
 
-setInterval(updateDateTime, 1000);
+function updateStopwatchDisplay() {
+  const hours = Math.floor(stopwatchTime / 3600);
+  const minutes = Math.floor((stopwatchTime % 3600) / 60);
+  const seconds = stopwatchTime % 60;
+  document.getElementById("stopwatchDisplay").textContent = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+}
+
+function padZero(num) {
+  return num.toString().padStart(2, "0");
+}
