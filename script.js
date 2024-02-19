c7205a957598669aa63a8b317f4451633318e534
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-19 15:13:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let sys1 = false;
 function update() {
     const now = new Date();
     let hours = now.getHours();
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
     let ampm = '';
 
     if (sys1) {
@@ -14,6 +12,9 @@ function update() {
         ampm = '';
         hours = String(hours).padStart(2, '0');
     }
+
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
 
     const currtime = `${hours}:${minutes}:${seconds} ${ampm}`;
     document.getElementById('sys').textContent = sys1 ? '12hr' : '24hr';
