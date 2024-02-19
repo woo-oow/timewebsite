cb41f57e0a39bfa0b5e966c70d86a17e54755a83
Wow <160201362+woo-oow@users.noreply.github.com>
2024-02-19 12:07:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let sys1 = false;
 function update() {
 
     const now = new Date();
-    let hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+    let hours = String(now.getHours());
+    const minutes = String(now.getMinutes());
+    const seconds = String(now.getSeconds());
     let currtime = `${hours}: ${minutes}: ${seconds}`;
 
     if (sys1) {
